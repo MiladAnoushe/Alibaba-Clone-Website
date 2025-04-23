08935bdc14d81c68b5f7c06e0f8f2c8870cb1beb
MiladAnoushe <milad1job@gmail.com>
2025-04-23 20:43:45 +0330

    responsive update for diffrent size and tabbar js update

--- Alibaba-js.js
+++ Alibaba-js.js
@@ -10,10 +10,10 @@ window.addEventListener("scroll", function () {
 
 var buttons = document.querySelectorAll(".navbar-dropdown-button");
 
-buttons.forEach(function(button) {
+buttons.forEach(function (button) {
   var dropdown = button.querySelector(".navbar-div-dropdown");
 
-  button.addEventListener("click", function() {
+  button.addEventListener("click", function () {
     if (dropdown.style.display === "none" || dropdown.style.display === "") {
       dropdown.style.display = "flex";
     } else {
@@ -21,3 +21,33 @@ buttons.forEach(function(button) {
     }
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".desktop-navbar-services");
+  const underline = container.querySelector(".underline");
+  const items = container.querySelectorAll(".item");
+
+  function moveUnderline(el) {
+    underline.style.width = el.offsetWidth + "px";
+    underline.style.left = el.offsetLeft + "px";
+  }
+
+  items.forEach(i => {
+    i.addEventListener("click", e => {
+      e.preventDefault();
+
+      items.forEach(x => x.classList.remove("active-tab"));
+      
+      i.classList.add("active-tab");
+
+      moveUnderline(i);
+    });
+  });
+
+  moveUnderline(container.querySelector(".active-tab"));
+});
+
+
+
+
